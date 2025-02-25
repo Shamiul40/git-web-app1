a9d896d6b6b215a5190e967283e2958f90f1627a
Shamiul Islam <shmaiul4040@gmail.com>
2025-02-25 16:58:00 +0600

    log in form done

--- script/apps.js
+++ script/apps.js
@@ -1,8 +1,26 @@
 
 document.getElementById("login-btn")
 .addEventListener("click", (event)=>{
-event.preventDefault();
+
+  event.preventDefault();
+
 const accountNumber = document.getElementById("account").value;
 const pin = document.getElementById("pin").value;
+const convertAccountNumber = parseInt(accountNumber.length);
+const convertPin = parseInt(pin);
+
+if(convertAccountNumber=== 11) {
+  
+  if(convertPin === 1234) {
+    console.log("thik ase");
+  }
+  else {
+    alert("invalid pin");
+  }
+}
+
+else {
+  alert("number invalid")
+}
 
 })
