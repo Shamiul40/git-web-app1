6589639141f3c2dbcfc9d1e47c9edcabe67a8229
Shamiul40 <shamiul4040@gmail.com>
2025-02-25 11:19:30 +0600

    f

---
+++ script/apps.js
@@ -0,0 +1,8 @@
+
+document.getElementById("login-btn")
+.addEventListener("click", (event)=>{
+event.preventDefault();
+const accountNumber = document.getElementById("account").value;
+const pin = document.getElementById("pin").value;
+
+})
